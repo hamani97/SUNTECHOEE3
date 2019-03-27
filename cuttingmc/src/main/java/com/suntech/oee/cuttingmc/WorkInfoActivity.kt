97288f523cdf2381ee29f821e33055dcf9994e8e
hamani97 <hamani@naver.com>
2019-03-28 08:09:03 +0900

    home note

--- cuttingmc/src/main/java/com/suntech/oee/cuttingmc/WorkInfoActivity.kt
+++ cuttingmc/src/main/java/com/suntech/oee/cuttingmc/WorkInfoActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
